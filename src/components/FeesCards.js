e498f7a0509a81eb3a2bad848ccb2b9c35ddbc4c
ananducv <153972540+anandude@users.noreply.github.com>
2024-10-03 17:05:11 +0530

    set the layout for schoolfees page including cards

---
+++ src/components/FeesCards.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FeesCards = () => {
+  return (
+    <div className='fees-card-wrapper'>
+        <h4>KG Classes</h4>
+        <p><span>₹ 3650</span>/ term</p>
+        <img src='/images/Price-KG.jpg' alt='a classroom of kg students'></img>
+    </div>
+  )
+}
+
+export default FeesCards
--- src/pages/SchoolFees.js
+++ src/pages/SchoolFees.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import BreadCrumb from '../components/BreadCrumb'
+import FeesCards from '../components/FeesCards'
 
 const SchoolFees = () => {
   return (
     <div>
         <BreadCrumb pagename={'School Fees'}/>
+        <div className='fees-wrapper'>
+        <div className='fees-text-container'>
+        <p>The school fee is to be remitted in four instalments through Banking System or School Office directly. Fee can be paid in full or in four instalments on the specified date as mentioned in the school dairy.</p>
+        </div>
+        <h3>Fee structure per instalment</h3>
+        <div className='fees-cards-flex'>
+        <FeesCards />
+        <FeesCards />
+        <FeesCards />
+        </div>
+        </div>
     </div>
   )
 }
