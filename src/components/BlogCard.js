daed3296583d565816c7dc4be64bd2906efa3e1b
ananducv <153972540+anandude@users.noreply.github.com>
2024-10-10 17:10:03 +0530

    updated the card info to annual day

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+
+// https://img.freepik.com/free-vector/futuristic-science-lab-background_23-2148495020.jpg?t=st=1728229546~exp=1728233146~hmac=5d458a6b808e0c510548aef0446697c3d452564def915e03e892d0ab604ca1ad&w=996"
+//                 alt="animated_card" className="w-full h-[200px] rounded-t-md object-cover
+
 const BlogCard = () => {
   return (
     <div>
@@ -10,14 +14,13 @@ const BlogCard = () => {
 
             {/*  image  */}
             <img
-                src="https://img.freepik.com/free-vector/futuristic-science-lab-background_23-2148495020.jpg?t=st=1728229546~exp=1728233146~hmac=5d458a6b808e0c510548aef0446697c3d452564def915e03e892d0ab604ca1ad&w=996"
-                alt="animated_card" className="w-full h-[200px] rounded-t-md object-cover"/>
+                src="/images/an_image_of_annual_day_celebrations_of_an_indian_s.png " alt="animated_card" className='w-full h-[200px] rounded-t-md object-cover
+'/>
 
             {/*  contents  */}
             <div className="p-[18px] pt-2.5 bg-white rounded-b-md">
-                <h3 className="text-[1.5rem] font-bold text-[#0FABCA]">Science</h3>
-                <p className="text-[1rem] font-[400] text-gray-600">The science of today is the
-                    technology of tomorrow – Edward Teller</p>
+                <h3 className="text-[1.5rem] font-bold text-[#0FABCA]">Annual Day</h3>
+                <p className="text-[1rem] font-[400] text-gray-600">The school Annual day celebrations took place on December 2nd 2023. The program was inaugrated by Actor Tom Cruise</p>
 
                 <button
                     className="w-full py-2 px-4 hover:bg-[#c0e6ed] hover:text-black text-[1rem] transition-all duration-300 bg-[#0FABCA] text-white rounded-md mt-5">Read More...
