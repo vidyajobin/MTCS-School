661c0e3487589daffa89df971cb9458c741bba4e
ananducv <153972540+anandude@users.noreply.github.com>
2024-11-06 17:58:31 +0530

    kindergarten and primary and secondary section covered

--- src/pages/Academics.js
+++ src/pages/Academics.js
@@ -4,7 +4,7 @@ import AcademicsCard from '../components/AcademicsCard'
 
 const Academics = () => {
 
-  const cardData = [
+  const cardData1 = [
     {
       image:"/images/course-2.jpg",
       heading: "Pre-KG",
@@ -15,6 +15,82 @@ const Academics = () => {
           text: 'This program concentrates on kids having an enhancing knowledge through fun and play. Tuning in to stories, number ideas, pre-composing and phonic aptitudes help create dialect and vocabulary. Apart from this exercises, yoga, music will aid in the holistic development. Formal pre-writing literacy and numeracy activities are also taught.'
         },        
       
+    },
+    {
+      image:"/images/course-2.jpg",
+      heading: "Lower Kindergarden (LKG)",
+      subheading: "9:00am - 3:45pm",
+      content: 
+        {
+          type: 'text',
+          text: `Our LKG programme is equipped with the best syllabus to suffice all your child's intellectual and physical needs and prepare them to recive the furture education that will be imparted to them inthe right way.Reading writing and speaking become easy for kids with the help of our advanced curriculum devised with the input of our trained teachers.`
+        },        
+      
+    },
+    {
+      image:"/images/course-2.jpg",
+      heading: "Upper Kindergarden(UKG)",
+      subheading: "9:00am - 3:45pm",
+      content: 
+        {
+          type: 'text',
+          text: `UKG is the initial level that is accomblished with social behaviour which is essential for a student to learn in a school .UKG includes all the basic manners like how to sit in class, how to talk to the teachers and how to respond to the situation in school manner and now the student is completely out of their kid zone. UKG syllabus is larger than LKG as the student jumps for initial level to primary level and it quite obvious.`
+        },        
+      
+    },
+    // Add more card data as needed
+  ];
+
+  const cardData2 = [
+    {
+      image:"/images/course-2.jpg",
+      heading: "Primary School",
+      subheading: "9.00am - 3.45pm",
+      content: [
+        {
+          type: 'text',
+          text: 'Primary section includes Class I - V. The syllabus followed is as per CBSE scheme. The subjects taught are:'
+        },
+        {
+          type: 'bullet',
+          items: [
+            "English",
+  "Hindi / Malayalam",
+  "Mathematics",
+  "Science",
+  "Social science",
+  "EVS",
+  "Physical and Health Education",
+  "General Knowledge",
+  "Moral Science / Value Education"
+          ]
+        }
+      ]
+    },
+    {
+      image:"/images/course-2.jpg",
+      heading: "High School",
+      subheading: "9.00am - 3.45pm",
+      content: [
+        {
+          type: 'text',
+          text: 'The curriculum for Classes IX and X is based on CBSE syllabus. The scholastic subjects are:'
+        },
+        {
+          type: 'bullet',
+          items: [
+            "English",
+  "Hindi / Malayalam",
+  "Mathematics",
+  "Science",
+  "Social science",
+  "Physical and Health Education",
+  "General Knowledge",
+  "Moral Science / Value Education",
+  "Computer Education"
+          ]
+        }
+      ]
     },
     {
       image:"/images/course-2.jpg",
@@ -43,6 +119,7 @@ const Academics = () => {
     },
     // Add more card data as needed
   ];
+
   
 
   return (
@@ -55,9 +132,31 @@ const Academics = () => {
         content={cardData}
         /> */}
 
-        {/* Use map() to iterate over each object in cardData */}
-        <div className='flex'>
-      {cardData.map((card, index) => (
+      <h1 className='text-5xl font-bold mb-5 mt-16'><span style={{color: '#5d50c6'}}>The</span> <span style={{color: 'orange'}}>Kindergarden</span></h1>
+        <div className='flex justify-center '>
+        <p className='mb-9 w-2/3 text-center' style={{color:'rgba(0, 0, 0, 0.5)'}}>The curriculum that we offer here is a happy blend of child oriented and teacher centred methodologies, focusing on to play way method. The aim is the development of total personality of the child.The Kindergarden is a preschool educational approch based on playing ,singing,practical activities such as drawing,and social intraction as part of the transition from home to school</p>
+        </div>
+
+        <div className='flex flex-wrap justify-center mx-auto'>
+      {cardData1.map((card, index) => (
+        
+        <AcademicsCard
+          key={index}              // Unique key for each card
+          imageUrl={card.image}  // Pass image URL for each card
+          heading={card.heading}    // Pass heading for each card
+          subheading={card.subheading} // Pass subheading for each card
+          content={card.content}    // Pass content for each card
+        />
+      ))}
+      </div>
+
+      <h1 className='text-5xl font-bold mb-5 mt-16'><span style={{color: '#5d50c6'}}>The</span> <span style={{color: 'orange'}}>Primary and Secondary School</span></h1>
+        <div className='flex justify-center '>
+        <p className='mb-9 w-2/3 text-center' style={{color:'rgba(0, 0, 0, 0.5)'}}>For classes I to X, the school follows the uniform system of Assessment, Examination, and Report Card recommended by CBSE for all its affiliated schools.The assessment is based on both scholastic and co- scholastic area. Academic sessions are divided into two terms, the First Term from June to October and the Second Term from November to March.</p>
+        </div>
+
+      <div className='flex flex-wrap justify-center mx-auto'>
+      {cardData2.map((card, index) => (
         
         <AcademicsCard
           key={index}              // Unique key for each card
@@ -69,12 +168,12 @@ const Academics = () => {
       ))}
       </div>
         
-        {/* <h1 className='text-5xl font-bold mb-5 mt-16'><span style={{color: '#5d50c6'}}>The</span> <span style={{color: 'orange'}}>Kindergarden</span></h1>
+        <h1 className='text-5xl font-bold mb-5 mt-16'><span style={{color: '#5d50c6'}}>The</span> <span style={{color: 'orange'}}>Kindergarden</span></h1>
         <div className='flex justify-center '>
         <p className='mb-9 w-2/3 text-center' style={{color:'rgba(0, 0, 0, 0.5)'}}>The curriculum that we offer here is a happy blend of child oriented and teacher centred methodologies, focusing on to play way method. The aim is the development of total personality of the child.The Kindergarden is a preschool educational approch based on playing ,singing,practical activities such as drawing,and social intraction as part of the transition from home to school</p>
         </div>
 
-        <div className='AcademicsCardContainer flex flex-wrap justify-center mx-auto'>
+        {/*  <div className='AcademicsCardContainer flex flex-wrap justify-center mx-auto'>
         <AcademicsCard 
         imageUrl="/images/course-1.jpg?height=480&width=440"
         heading="Lower Kindergarden (LKG)"
